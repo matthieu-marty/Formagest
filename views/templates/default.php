b9ef8c86975dea9cc78809850b412d4d745b6767
Matthieu MARTY <matthieu.marty@hotmail.com>
2021-06-24 11:27:35 +0200

    Fix | Zoom sur interface mobile

--- views/templates/default.php
+++ views/templates/default.php
@@ -5,10 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
-    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
     <link rel="icon" href="<?= URL ?>public/icon/icon.png" type="image/png">
     <link rel="stylesheet" href="<?= URL ?>public/style/formagest.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
 </head>
 
