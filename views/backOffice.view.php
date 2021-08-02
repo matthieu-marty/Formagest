bb924fd7ab4c77eff301b00d6d40c94f5c62364d
Matthieu MARTY <matthieu.marty@hotmail.com>
2021-08-02 17:18:12 +0200

    Style |  Carte cliquable

--- views/backOffice.view.php
+++ views/backOffice.view.php
@@ -6,77 +6,72 @@ ob_start()
 <h3>Back Office</h3>
 
 <div class="grid gap-2 mb-2">
-    <div class="card border-blue-hover col-lg-4 col-md-6 col-sm-12">
+
+    <a href="<?= URL ?>entreprises" class="card col-lg-4 col-md-6 col-sm-12">
         <h3>Entreprises</h3>
         <div class="flex-row mb-2">
             <img src="https://img.icons8.com/color-glass/100/000000/company.png" />
             <span style="font-size: 4rem; font-weight:200;padding-left:1.5rem;margin-left: 1.5rem; border-left: 2px solid var(--border);"><?= $stat->getNbEntreprise() ?></span>
         </div>
-        <a href="<?= URL ?>entreprises" class="btn btn-primary-action">Voir plus</a>
-    </div>
+    </a>
 
-    <div class="card border-blue-hover col-lg-4 col-md-6 col-sm-12">
+    <a href="<?= URL ?>organismes-de-formation" class="card col-lg-4 col-md-6 col-sm-12">
         <h3>Organismes de formation</h3>
         <div class="flex-row mb-2">
             <img src="https://img.icons8.com/color-glass/100/000000/school.png" />
             <span style="font-size: 4rem; font-weight:200;padding-left:1.5rem;margin-left: 1.5rem; border-left: 2px solid var(--border);"><?= $stat->getNbOrganismeFormation() ?></span>
         </div>
-        <a href="<?= URL ?>organismes-de-formation" class="btn btn-primary-action">Voir plus</a>
-    </div>
+    </a>
 
-    <div class="card border-blue-hover col-lg-4 col-md-6 col-sm-12">
+    <a href="<?= URL ?>opcas" class="card col-lg-4 col-md-6 col-sm-12">
         <h3>OPCAs</h3>
         <div class="flex-row mb-2">
             <img src="https://img.icons8.com/color-glass/100/000000/money-transfer.png" />
             <span style="font-size: 4rem; font-weight:200;padding-left:1.5rem;margin-left: 1.5rem; border-left: 2px solid var(--border);"><?= $stat->getNbOpca() ?></span>
         </div>
-        <a href="<?= URL ?>opcas" class="btn btn-primary-action">Voir plus</a>
-    </div>
+    </a>
 
-    <div class="card border-blue-hover col-lg-4 col-md-6 col-sm-12">
+    <a href="#" class="card col-lg-4 col-md-6 col-sm-12">
         <h3>Programmes de formations</h3>
 
         <div class="flex-row mb-2">
             <img src="https://img.icons8.com/color-glass/100/000000/copybook.png" class="d-block" />
             <span style="font-size: 4rem; font-weight:200;padding-left:1.5rem;margin-left: 1.5rem; border-left: 2px solid var(--border);"><?= $stat->getNbProgramme() ?></span>
         </div>
-        <a href="#" class="btn btn-primary-action">Voir plus</a>
-    </div>
+    </a>
 
-    <div class="card border-blue-hover col-lg-4 col-md-6 col-sm-12">
+    <a href="<?= URL ?>modules" class="card col-lg-4 col-md-6 col-sm-12">
         <h3>Modules</h3>
 
         <div class="flex-row mb-2">
             <img src="https://img.icons8.com/color-glass/100/000000/module.png" class="d-block" />
             <span style="font-size: 4rem; font-weight:200;padding-left:1.5rem;margin-left: 1.5rem; border-left: 2px solid var(--border);"><?= $stat->getNbModule() ?></span>
         </div>
-        <a href="<?= URL ?>modules" class="btn btn-primary-action">Voir plus</a>
-    </div>
-    <div class="card border-blue-hover col-lg-4 col-md-6 col-sm-12">
+    </a>
+
+    <a href="#" class="card col-lg-4 col-md-6 col-sm-12">
         <h3>Effectifs</h3>
         <div class="flex-row mb-2">
             <img src="https://img.icons8.com/color-glass/100/000000/conference.png" class="d-block" />
             <span style="font-size: 4rem; font-weight:200;padding-left:1.5rem;margin-left: 1.5rem; border-left: 2px solid var(--border);"><?= $stat->getNbEffectif() ?></span>
         </div>
-        <a href="#" class="btn btn-primary-action">Voir plus</a>
-    </div>
-    <div class="card border-blue-hover col-lg-4 col-md-6 col-sm-12">
+    </a>
+    
+    <a href="<?= URL ?>formateurs" class="card col-lg-4 col-md-6 col-sm-12">
         <h3>Formateurs</h3>
         <div class="flex-row mb-2">
             <img src="https://img.icons8.com/color-glass/100/000000/training.png" class="d-block" />
             <span style="font-size: 4rem; font-weight:200;padding-left:1.5rem;margin-left: 1.5rem; border-left: 2px solid var(--border);"><?= $stat->getNbFormateur() ?></span>
         </div>
-        <a href="<?= URL ?>formateurs" class="btn btn-primary-action">Voir plus</a>
-    </div>
+    </a>
 
-    <div class="card border-blue-hover col-lg-4 col-md-6 col-sm-12">
+    <a href="<?= URL ?>salles" class="card col-lg-4 col-md-6 col-sm-12">
         <h3>Salles</h3>
         <div class="flex-row mb-2">
             <img src="https://img.icons8.com/color-glass/100/000000/point-objects.png" class="d-block" />
             <span style="font-size: 4rem; font-weight:200;padding-left:1.5rem;margin-left: 1.5rem; border-left: 2px solid var(--border);"><?= $stat->getNbSalle() ?></span>
         </div>
-        <a href="<?= URL ?>salles" class="btn btn-primary-action">Voir plus</a>
-    </div>
+    </a>
 
 </div>
 
--- views/dossiersDeFormation.view.php
+++ views/dossiersDeFormation.view.php
@@ -5,35 +5,32 @@ ob_start()
 <h3>Dossiers de formation</h3>
 
 <div class="grid gap-2">
-    <div class="card border-blue-hover col-lg-4 col-md-6 col-sm-12">
+    <a href="<?= URL ?>montage-dossier" class="card border-blue-hover col-lg-4 col-md-6 col-sm-12">
         <h3>Montage dossiers</h3>
-        <img src="https://img.icons8.com/color-glass/100/000000/live-folder.png" class="d-block mb-2" />
-        <a href="<?= URL ?>montage-dossier" class="btn btn-primary-action">Créer un nouveau dossier</a>
-    </div>
+        <img src="https://img.icons8.com/color-glass/100/000000/live-folder.png" class="d-block" />
 
-    <div class="card border-blue-hover col-lg-4 col-md-6 col-sm-12">
+    </a>
+
+    <a href="#" class="card border-blue-hover col-lg-4 col-md-6 col-sm-12">
         <h3>Dossiers cloturés</h3>
-        <div class="flex-row mb-2">
+        <div class="flex-row">
             <img src="https://img.icons8.com/color-glass/100/000000/package.png" />
             <span style="font-size: 4rem; font-weight:200;padding-left:1.5rem;margin-left: 1.5rem; border-left: 2px solid var(--border);">65</span>
         </div>
-        <a href="#" class="btn btn-primary-action">Historique</a>
-    </div>
+    </a>
 
-    <div class="card border-blue-hover col-lg-4 col-md-6 col-sm-12">
+    <a href="#" class="card border-blue-hover col-lg-4 col-md-6 col-sm-12">
         <h3>Planning de formation</h3>
-        <img src="https://img.icons8.com/color-glass/100/000000/tear-off-calendar.png" class="d-block mb-2" />
-        <a href="#" class="btn btn-primary-action">Planning de formation</a>
-    </div>
+        <img src="https://img.icons8.com/color-glass/100/000000/tear-off-calendar.png" class="d-block" />
+    </a>
 
-    <div class="card border-blue-hover col-lg-4 col-md-6 col-sm-12">
+    <a href="<?= URL ?>dossiers-en-cours" target="_blank" class="card border-blue-hover col-lg-4 col-md-6 col-sm-12">
         <h3>Dossiers en cours</h3>
-        <div class="flex-row mb-2">
+        <div class="flex-row">
             <img src="https://img.icons8.com/color-glass/100/000000/refresh-folder.png" class="d-block" />
             <span style="font-size: 4rem; font-weight:200;padding-left:1.5rem;margin-left: 1.5rem; border-left: 2px solid var(--border);">65</span>
         </div>
-        <a href="<?= URL ?>dossiers-en-cours" target="_blank" class="btn btn-primary-action">Tableau de suivi</a>
-    </div>
+    </a>
 
 </div>
 
