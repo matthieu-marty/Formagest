407ab5ab3d8754b258ee8da36914d82eb67d4da1
Matthieu MARTY <matthieu.marty@hotmail.com>
2021-09-07 22:20:23 +0200

    Feat | doc formation #1

--- views/prints/conventionDeFormation.print.php
+++ views/prints/conventionDeFormation.print.php
@@ -8,6 +8,7 @@ ob_start()
             <p class="title">CONVENTION DE FORMATION</h3>
             <p class="title">Entre [FORMATEUR] et [STAGIAIRE]</h4>
             <p class="details">Client: [REF_CLIENT] - Ref: [TC433-21A11]</p>
+            <p class="details text-right">Page 1/3</p>
         </div>
         <div class="grid">
             <div class="c1-6">
@@ -69,25 +70,135 @@ ob_start()
 
 <div class="page">
     <div class="subpage">
-        <p class="title">CONVENTION DE FORMATION</h3>
-        <p class="title">Entre [FORMATEUR] et [STAGIAIRE]</h4>
-        <p class="details">Client: [REF_CLIENT] - Ref: [TC433-21A11]</p>
+        <div class="header">
+            <p class="title">CONVENTION DE FORMATION</h3>
+            <p class="title">Entre [FORMATEUR] et [STAGIAIRE]</h4>
+            <p class="details">Client: [REF_CLIENT] - Ref: [TC433-21A11]</p>
+            <p class="details text-right">Page 2/3</p>
+        </div>
+
+        <p class="article">Article - 1 Descriptif de l'action (suite)</p>
+        <p class="bold">Modalité de déroulement de la formation</p>
+        <p>La méthode est basée sur une alternance de théorie,
+            d'exercices pratiques et de démonstrations tenant
+            compte des connaissances de chaque stagiaire, de leur
+            rythme et des exigences de leur poste de travail.</p>
+
+        <p class="bold">Nature de la sanction de l'action de formation :</p>
+        <p>A la fin de la formation, il sera remis au stagiaire une attestation de stage.
+            Les modalités d'évaluation des acquis se basent sur le contrôle continu des éléments vus
+            tout au long de la formation (Quiz et QCM, jeu de rôle...). Une attestation des résultats de cette évaluation,
+            une attestation d'assisuité et une copie de la feuille d'émargement seront remises au stagiaire.
+        </p>
+
+        <p class="article">Article 2 - Délai de rétractation</p>
+        <p>[ENTREPRISE] bénéficie d'un délai de rétractation allant jusqu'à 10 jours francs à compter de la date de signature de la
+            présente convention, sans que ne lui soit demandé aucun frais de dédommagement. [ENTREPRISE] en informe [FORMATEUR] par lettre recommandé avec accusé de réception.
+        </p>
+        <p>
+            En cas de financement par un OPCA et sur présentation de la copie de demande de prise en
+            charge le délai de rétractation pourra être allongé à la date d'accord de prise en charge
+            de l'organisme avec un maximum de 30 jours francs après la signature de la présente convention.
+        </p>
+        <p class="article">Article 3 - La Facturation</p>
+        <p>En contrepartie de cette action de formation, le contractant s'engage à acquitter les frais suivants :</p>
+        <p>Frais de formation : coût unitaire H.T. [COUT] x [Q_STAGIAIRE] stagiaire(s) = [TOTAL] € H.T.</p>
+        <p>[FORMATEUR] n'est pas assujeti à la TVA selon l'article 261-4-4 du [TOTAL] TTC</p>
+
+        <p class="bold">S'engage à payer la prestation selon les modalités de paiement suivantes :</p>
+        <p>A moins d'une subrogation accordée par l'OPCA, [ENTREPRISE] effectue son règlement total à la remise de la facture par chèque à l'ordre de [FORMATEUR].
+            Les délais d'encaissements des règlements peuvent courir jusqu'à 60 jours fin de mois à compter de la date d'émission de la facture.
+            Si d’un commun accord entre les parties, le paiement de la prestation est échelonné un échéancier sera joint en annexe à la présente convention.
+            Les encaissements se feront dès lors a date fixe par chèque ou par virement à l'ordre de [FORMATEUR] . La facture globale sera remise au stagiaire en fin de formation
+            et reprendra l'ensemble des paiements effectués et/ou restants.</p>
+
+        <p class="article">Article 4 - Interruption de stage</p>
+        <p class="bold">Annulation de l'inscription ou cessation anticipée à l'iniative du participant</p>
+        <p>Les dates de formation sont fixées conjointement par [FORMATEUR] et[ENTREPRISE] lors de la signature de la Convention de formation professionnelle.
+            Toute annulation émanant d’un Participant doit faire l’objet d’un écrit (recommandé avec accusé de réception ou courriel) auprès de [FORMATEUR] .</p>
+
+        <p>. Si la force majeure n'est pas reconnue, les prestations déjà dispensées seront dûes et [FORMATEUR] pourra,
+            retenir des frais de dédommagement équivalents à 30% des heures non effectuées. Cette somme sera spécifiée sur
+            la facture distinctement des prestations réalisées au titre de la formation professionnelle.</p>
+        <p>. Si le Participant est empêché de suivre la formation par suite de force majeure dûment reconnue, seules les
+            prestations déjà dispensées sont dûes, au prorata des heures déjà effectuées.</p>
+
+        <p>. En cas d’annulation de force majeure ou dans un délai supérieur à 20 jours francs avant le début de la
+            formation et si le report sur une autre session est possible, il et limité à une seule fois dans l’année en
+            cours ; ceci étant, cet accord devra faire l’objet d’un écrit signé des deux parties et joint en annexe à la convention initiale.</p>
+
+        <p class="bold">Annulation de l'inscription ou cessation anticipée à l'iniative de</p>
+        <p> Dans le cas où le nombre de participants serait insuffisant pour assurer le déroulement d’une formation, se réserve la possibilité
+            d’annuler une session une semaine avant son échéance, sans dédommagement ni pénalité.
+        </p>
     </div>
 </div>
 
 <div class="page">
     <div class="subpage">
-        <p class="title">CONVENTION DE FORMATION</h3>
-        <p class="title">Entre [FORMATEUR] et [STAGIAIRE]</h4>
-        <p class="details">Client: [REF_CLIENT] - Ref: [TC433-21A11]</p>
-    </div>
-</div>
+        <div class="header">
+            <p class="title">CONVENTION DE FORMATION</h3>
+            <p class="title">Entre [FORMATEUR] et [STAGIAIRE]</h4>
+            <p class="details">Client: [REF_CLIENT] - Ref: [TC433-21A11]</p>
+            <p class="details text-right">Page 2/3</p>
+        </div>
 
-<div class="page">
-    <div class="subpage">
-        <p class="title">CONVENTION DE FORMATION</h3>
-        <p class="title">Entre [FORMATEUR] et [STAGIAIRE]</h4>
-        <p class="details">Client: [REF_CLIENT] - Ref: [TC433-21A11]</p>
+        <p class="article">Article 5 - Différents éventuels</p>
+        <p>La présente convention prend effet à compter de sa signature par l’entreprise.
+            Si une contestation ou un différent ne peuvent être réglés à l'amiable, le Tribunal de Grande Instance de TARBES sera seul compétent pour régler le litige.
+        </p>
+
+        <p class="article">Article 6 - Informations à communiquer au stagiaire avant signature de la convention</p>
+        <p>Par la signature de la présente convention, [ENTREPRISE] et [FORMATEUR] attestent sur l’honneur que ses stagiaires ont été informés des
+            conditions d’accès et prérequis à la formation mentionnées à l’article 2 de la présente convention.</p>
+
+        <p class="bold">Le stagiaire atteste également qu'il lui a été remis, avant son inscription définitive :</p>
+        <p>- Les tarifs et conditions générales de vente</p>
+        <p>- Le programme et les objectifs de la formation</p>
+        <p>- Les horaires prévus</p>
+        <p>- La liste du ou des formateur(s) avec indication de leurs titres et/ou qualités</p>
+        <p>- Les modalités d’évaluation et de sanction de la formation</p>
+        <p>- Les coordonnées de la personne chargée des relations avec les stagiaires</p>
+        <p>- Le règlement intérieur applicable à la formation</p>
+        <p>- Le livret du stagiaire</p>
+        <p>Ces informations peuvent-être obtenue par demande mail ou directement sur le site www.liberty-progress.fr/téléchargement</p>
+
+        <p class="article">Article 7 : Protection des données personnelles du client</p>
+        <p>Par la signature de la présente convention, le stagiaire atteste que l'ensemble des données personnelles en possession de et des prestataires pouvant intervenir en son nom, ont été obtenues avec son consentement.</p>
+        <p>Les informations recueillies par [FORMATEUR] et les prestataires pouvant intervenir en son nom font l’objet d’un traitement informatique destiné à assurer la gestion administrative, financière et pédagogique du stagiaire dans le cadre de cette convention de formation professionnelle.</p>
+        <p>Seules les personnes habilitées des services administratifs et comptables de [FORMATEUR], des organismes paritaires collecteurs agrées
+            (OPCA) et services de contrôle de la formation professionnelle (DIRECCTE) auront accès à vos données. Les données vous concernant
+            sont conservées au maximum 10 ans conformement à la règlementation de contrôle de la formation professionnelle. Au-delà, toutes les
+            données sont détruites dans un délais de 1 an.</p>
+        <p>Conformément à la loi « informatique et libertés » du 6 janvier 1978 modifiée en 2004, vous bénéficiez d’un droit
+            d’accès et de rectification aux informations qui vous concernent, que vous pouvez exercer en vous adressant à [FORMATEUR].
+            Par ailleur [FORMATEUR] et les prestataires pouvant intervenir en son nom s'engagent à respecter la confidentialité des
+            données de l'entreprise et des stagiaires échangées en dehors ou durant la formation.</p>
+
+        <p class="article">Article 8 : Le droit à l'image</p>
+        <p> Par la signature de la présente convention, [ENTREPRISE] autorise à titre gratuit</p>
+        <p>- le(s) filmer /photographier/et ou enregistrer lors de de la réalisation de l'action de formation visée à l'article 1</p>
+        <p>- à effectuer un montage, reproduire et diffuser ces images/enregistrements lors de projections à but non lucratif.</p>
+        <p>- à publier ces images/voix sur le web. </p>
+        <p> Il est informé et atteste avoir informé son / ses stagiaire(s) de la possiblilité de se rétracter à tout moment sur simple demande écrite à via les coordonnées reprises en bas de page.</p>
+        <div class="grid mt-2">
+            <p class="c2-6 to-complete">Fait en double exemplaires,</p>
+            <p class="c2-6 to-complete">Le ........ / ........ / ...............</p>
+            <p class="c2-6 to-complete">à ..............................................</p>
+        </div>
+        <div class="grid">
+            <div class="c3-6 ">
+                <p class="bold text-center">Pour</p>
+                <p class="text-center">Le(la) responsable</p>
+                <p class="text-center">[STAGIAIRE]</p>
+
+            </div>
+            <div class="c3-6 text-center">
+                <p class="bold text-center">Pour</p>
+                <p class="text-center">Le(la) responsable</p>
+                <p class="text-center">[FORMATEUR]</p>
+            </div>
+        </div>
     </div>
 </div>
 
